2c1c416fc68b2c305cee884f6cc792509bb4b367
Eric Laursen <ericjlaursen@gmail.com>
2023-03-14 15:15:30 -0400

    adjust

--- routes/nlp/sentiment/post.js
+++ routes/nlp/sentiment/post.js
@@ -6,17 +6,18 @@
   - returns
     - array of objects: 
      { text: '...', sentiment: NumberHere }
-*/ 
+*/
 import natural from 'natural';
+import summaryObj from './summaryObj.js';
 
 function setupNLPTools() {
   // https://naturalnode.github.io/natural/sentiment_analysis.html
-  const { PorterStemmer, SentimentAnalyzer} = natural;
+  const { PorterStemmer, SentimentAnalyzer } = natural;
   const language = 'English';
 
   const SENTIMENT_TYPE = 'afinn';
   const affinityAnalyzer = new SentimentAnalyzer(language, PorterStemmer, SENTIMENT_TYPE);
-  return { affinityAnalyzer};
+  return { affinityAnalyzer };
 }
 
 function buildArrOfSentences(txt) {
@@ -32,42 +33,43 @@ function buildArrOfWords(s) {
 }
 
 function getLongestWord(arr) {
-  return arr.reduce((a, b) => a.length > b.length ? a : b)
+  return arr.reduce((a, b) => (a.length > b.length ? a : b));
 }
 
 export default function postHandler(req, res) {
   if (!req?.body?.text) {
-    throw new Error('expects a json request in the body with a "text" key, like this -> { text: `here`}')
+    throw new Error(
+      'expects a json request in the body with a "text" key, like this -> { text: `here`}'
+    );
   }
 
   const sentences = buildArrOfSentences(req.body.text);
 
-  let summary = {
+  let sentenceArr = [];
+  let internalSummary = {
     sentences: 0,
     agvWordsPerSentence: 0,
     words: 0,
     sentiments: {
       positive: {
         count: 0,
-        percent: 0
+        percent: 0,
       },
       negative: {
         count: 0,
-        percent: 0
+        percent: 0,
       },
       neutral: {
         count: 0,
-        percent: 0
+        percent: 0,
       },
-    }
-  }
-
-  let sentenceArr = []
-  sentences.forEach(s => {
+    },
+  };
+  sentences.forEach((s) => {
     const { affinityAnalyzer } = setupNLPTools();
     const thisSentenceWordTokens = buildArrOfWords(s);
     const sentScore = affinityAnalyzer.getSentiment(thisSentenceWordTokens);
-    
+
     // create sentence obj
     let thisObj = {};
     thisObj.sentence = s;
@@ -75,34 +77,37 @@ export default function postHandler(req, res) {
     thisObj.length = thisSentenceWordTokens.length;
     thisObj.longestWord = getLongestWord(thisSentenceWordTokens);
     sentenceArr.push(thisObj);
-
-    // update summary
-    summary.sentences = summary.sentences + 1;
-    summary.words = summary.words + thisObj.length
     
+    // update summary
+    internalSummary.sentences = internalSummary.sentences + 1;
+    internalSummary.words = internalSummary.words + thisObj.length;
+
     if (sentScore > 0) {
-      summary.sentiments.positive.count = summary.sentiments.positive.count + 1
+      internalSummary.sentiments.positive.count = internalSummary.sentiments.positive.count + 1;
     }
     if (sentScore < 0) {
-      summary.sentiments.negative.count = summary.sentiments.negative.count + 1;
+      internalSummary.sentiments.negative.count = internalSummary.sentiments.negative.count + 1;
     }
     if (sentScore == 0) {
-      summary.sentiments.neutral.count = summary.sentiments.neutral.count + 1;
+      internalSummary.sentiments.neutral.count = internalSummary.sentiments.neutral.count + 1;
     }
 
-    summary.sentiments.positive.percent = Math.round((summary.sentiments.positive.count / summary.sentences) * 100, 0)
-    summary.sentiments.negative.percent = Math.round(
-      (summary.sentiments.negative.count / summary.sentences) * 100,
+    internalSummary.sentiments.positive.percent = Math.round(
+      (internalSummary.sentiments.positive.count / internalSummary.sentences) * 100,
       0
     );
-    summary.sentiments.neutral.percent = Math.round(
-      (summary.sentiments.neutral.count / summary.sentences) * 100,
+    internalSummary.sentiments.negative.percent = Math.round(
+      (internalSummary.sentiments.negative.count / internalSummary.sentences) * 100,
+      0
+    );
+    internalSummary.sentiments.neutral.percent = Math.round(
+      (internalSummary.sentiments.neutral.count / internalSummary.sentences) * 100,
       0
     );
   });
-  
 
-  res.json({ summary, sentenceAnalysis: sentenceArr});
+  res.json({ summary: internalSummary, sentenceAnalysis: sentenceArr });
   sentenceArr = null;
+  internalSummary = null;
   return;
 }
---
+++ routes/nlp/sentiment/summaryObj.js
@@ -0,0 +1,21 @@
+const summary = {
+  sentences: 0,
+  agvWordsPerSentence: 0,
+  words: 0,
+  sentiments: {
+    positive: {
+      count: 0,
+      percent: 0,
+    },
+    negative: {
+      count: 0,
+      percent: 0,
+    },
+    neutral: {
+      count: 0,
+      percent: 0,
+    },
+  },
+};
+
+export default summary
