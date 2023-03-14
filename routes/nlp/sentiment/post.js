ffb048fdc07a9b7d3c0a1287169db0a50c69696e
Eric Laursen <ericjlaursen@gmail.com>
2023-03-13 20:22:04 -0400

    lots more logic here...

--- routes/nlp/sentiment/post.js
+++ routes/nlp/sentiment/post.js
@@ -35,29 +35,74 @@ function getLongestWord(arr) {
   return arr.reduce((a, b) => a.length > b.length ? a : b)
 }
 
-function prepSentenceObject(s) { 
-  const thisSentenceWordTokens = buildArrOfWords(s);
-  const sentScore = affinityAnalyzer.getSentiment(thisSentenceWordTokens);
-
-  let thisObj = new Map();
-  thisObj.set('sentence', s);
-  thisObj.set('sentimentScore', sentScore);
-  thisObj.set('length', thisSentenceWordTokens);
-  thisObj.set('longestWord', getLongestWord(s));
-
-  return thisObj;
-}
-
 export default function postHandler(req, res) {
   if (!req?.body?.text) {
     throw new Error('expects a json request in the body with a "text" key, like this -> { text: `here`}')
   }
 
-  const { affinityAnalyzer } = setupNLPTools();
-
   const sentences = buildArrOfSentences(req.body.text);
 
-  const sentenceObj = sentences.map(prepSentenceObject);
+  let summary = {
+    sentences: 0,
+    agvWordsPerSentence: 0,
+    words: 0,
+    sentiments: {
+      positive: {
+        count: 0,
+        percent: 0
+      },
+      negative: {
+        count: 0,
+        percent: 0
+      },
+      neutral: {
+        count: 0,
+        percent: 0
+      },
+    }
+  }
+
+  let sentenceArr = []
+  sentences.forEach(s => {
+    const { affinityAnalyzer } = setupNLPTools();
+    const thisSentenceWordTokens = buildArrOfWords(s);
+    const sentScore = affinityAnalyzer.getSentiment(thisSentenceWordTokens);
+    
+    // create sentence obj
+    let thisObj = {};
+    thisObj.sentence = s;
+    thisObj.sentimentScore = sentScore;
+    thisObj.length = thisSentenceWordTokens.length;
+    thisObj.longestWord = getLongestWord(thisSentenceWordTokens);
+    sentenceArr.push(thisObj);
+
+    // update summary
+    summary.sentences = summary.sentences + 1;
+    summary.words = summary.words + thisObj.length
+    
+    if (sentScore > 0) {
+      summary.sentiments.positive.count = summary.sentiments.positive.count + 1
+    }
+    if (sentScore < 0) {
+      summary.sentiments.negative.count = summary.sentiments.negative.count + 1;
+    }
+    if (sentScore == 0) {
+      summary.sentiments.neutral.count = summary.sentiments.neutral.count + 1;
+    }
+
+    summary.sentiments.positive.percent = Math.round((summary.sentiments.positive.count / summary.sentences) * 100, 0)
+    summary.sentiments.negative.percent = Math.round(
+      (summary.sentiments.negative.count / summary.sentences) * 100,
+      0
+    );
+    summary.sentiments.neutral.percent = Math.round(
+      (summary.sentiments.neutral.count / summary.sentences) * 100,
+      0
+    );
+  });
   
-  return res.json(sentenceObj);
+
+  res.json({ summary, sentenceAnalysis: sentenceArr});
+  sentenceArr = null;
+  return;
 }
