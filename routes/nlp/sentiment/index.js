ceda4212453382fc135c84e63d41e0d9696f997f
Eric Laursen <ericjlaursen@gmail.com>
2023-03-14 19:51:05 -0400

    mock excel handler

--- routes/nlp/sentiment/index.js
+++ routes/nlp/sentiment/index.js
@@ -2,6 +2,10 @@ import express from 'express';
 import postHandler from './post.js';
 const sentimentRouter = express.Router();
 
+function excelPost(req, res) {
+  return res.send('excel hurr')
+}
+sentimentRouter.post('/excel', excelPost);
 sentimentRouter.post('/', postHandler);
 
 export default sentimentRouter;
