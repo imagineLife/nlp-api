75c66fd7f99be51acd5a594bfe4a5ee4d8736408
Eric Laursen <ericjlaursen@gmail.com>
2023-03-29 07:51:10 -0400

    change setup to be sync

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+import { config as dotenvConfig } from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { expressSetup, setupDB } from './setup/index.js';
-import { buildThemes } from './lib/index.js'
+import { buildThemes } from './lib/index.js';
 
-const dbObj = {
-  username: process.env.MONGO_DB_USER,
-  pw: process.env.MONGO_DB_PW,
-  host: process.env.MONGO_DB_HOST,
-  port: process.env.MONGO_DB_PORT,
-  authDB: process.env.MONGO_DB_AUTH_DB,
-};
-const MongoClient = await setupDB({ ...dbObj });
+async function setup() { 
+  dotenvConfig();
+  console.log('ENV: config done');
+  await setupDB();
+  buildThemes();
+  expressSetup();
+} 
 
-setupDB()
-buildThemes()
-expressSetup();
+setup()
