35a223aa8f4664748308d6b9d30a229fcc751a1d
Eric Laursen <ericjlaursen@gmail.com>
2023-03-31 20:12:14 -0400

    adjust init

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import { config as dotenvConfig } from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 import { expressSetup, setupDB } from './setup/index.js';
 import { buildThemes } from './lib/index.js';
+import { Crud } from './lib/models/crud/index.js'
+import { stateObj } from './state.js';
 
 async function setup() { 
   dotenvConfig();
   console.log('ENV: config done');
-  await setupDB();
+  const DbClient = await setupDB();
+  const NlpDb = DbClient.registerDB("Nlp");
+  const SpeechesCollection = new Crud({
+    db: NlpDb,
+    collection: 'Speeches'
+  });
+  stateObj.Collections.Speeches = SpeechesCollection;
+
   buildThemes();
   expressSetup();
 } 
