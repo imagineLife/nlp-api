9b5df9741040d20d20f5c23ebdc7823666cdca77
Eric Laursen <ericjlaursen@gmail.com>
2023-04-02 12:03:08 -0400

    init speech post to db

--- routes/api/speeches/index.js
+++ routes/api/speeches/index.js
@@ -1,12 +1,25 @@
-import { Router } from "express";
-
-async function getSpeeches(req,res) {
-  return res.status(200).json({get: 'speeches'})
+import { Router } from 'express';
+import { stateObj } from './../../../state.js';
+async function getSpeeches(req, res) {
+  return res.status(200).json({ get: 'speeches' });
 }
 
-function postASpeech(req, res) { 
-  return res.status(200).json({posted: 'demo'})
+async function postASpeech(req, res) {
+  // sanity checking
+  const {
+    body: { author, text, date },
+  } = req;
+  if (!author || !text || !date) {
+    return res.status(422).json({ Error: 'missing required params' });
+  }
+
+  try {
+    const { insertedId } = await stateObj.Collections.Speeches.createOne({ author, text, date });
+    return res.set('Location', `/speeches/${insertedId}`).status(200).end();
+  } catch (error) {
+    return res.status(500).json({ Error: error.message });
+  }
 }
-const speechesRouter = Router()
-speechesRouter.get('/', getSpeeches).post('/', postASpeech)
+const speechesRouter = Router();
+speechesRouter.get('/', getSpeeches).post('/', postASpeech);
 export default speechesRouter;
