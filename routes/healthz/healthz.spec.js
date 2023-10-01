3a749aa116ac4feee52b6fadf890b1b8532fc70c
Eric Laursen <ericjlaursen@gmail.com>
2023-09-30 20:55:16 -0400

    readiness

--- routes/healthz/healthz.spec.js
+++ routes/healthz/healthz.spec.js
@@ -1,6 +1,7 @@
 import supertest from 'supertest';
 import { expressSetup, serverKiller } from './../../setup/index.js';
 const LIVENESS_URL = '/healthz/liveness'
+const READINESS_URL = '/healthz/readiness';
 describe('GET /healthz', () => {
   let app;
   beforeAll(() => { 
@@ -11,9 +12,15 @@ describe('GET /healthz', () => {
     
     app.close();
   })
-  it(LIVENESS_URL, async () => {
+  it(`${LIVENESS_URL} returns a 200 with text "OK"`, async () => {
     const { statusCode, text } = await supertest(app).get(LIVENESS_URL);
     expect(statusCode).toBe(200);
     expect(text).toBe('OK');
+  });
+  it(`${READINESS_URL} returns a 200 with body status and fields`, async () => {
+    const { statusCode, body: { status, timestamp } } = await supertest(app).get(READINESS_URL);
+    expect(statusCode).toBe(200);
+    expect(status).toBe('OK');
+    expect(typeof timestamp).toBe('string')
   });
 })
