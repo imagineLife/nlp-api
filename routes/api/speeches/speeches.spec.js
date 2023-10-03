91fd880be1e24bb62cb7e5ec269e6489be7fe04b
Eric Laursen <ericjlaursen@gmail.com>
2023-10-03 19:45:35 -0400

    another on bites the test

---
+++ routes/api/speeches/speeches.spec.js
@@ -0,0 +1,26 @@
+import supertest from 'supertest';
+import { expressSetup } from './../../../setup/index.js';
+import { registerDbCollections } from './../../../setup/db.js';
+import setupTestDB from './../../../lib/config/setupTestDb.js';
+describe('speeches byId', () => {
+  const GET_SPEECHES_URL = '/api/speeches';
+  let app, dbClient;
+  beforeAll(async () => {
+    app = expressSetup();
+    const { MongoClient } = await setupTestDB();
+    dbClient = MongoClient;
+    registerDbCollections(dbClient);
+  });
+  afterAll(() => {
+    console.log('closing app');
+    app.close();
+    dbClient.close()
+  });
+
+  it('GET', async () => {
+    const { statusCode, body } = await supertest(app).get(GET_SPEECHES_URL);
+    expect(statusCode).toBe(200);
+    console.log('body')
+    console.log(body)
+  });
+});
