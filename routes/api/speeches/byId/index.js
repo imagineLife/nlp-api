6780dc51ac90d05b5ece82296f4c58d6ca6dee28
Eric Laursen <ericjlaursen@gmail.com>
2023-10-19 20:02:25 -0400

    more tests, more functionality

--- routes/api/speeches/byId/index.js
+++ routes/api/speeches/byId/index.js
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import { getById } from './get.js';
 const speechByIdRouter = Router({ mergeParams: true });
 
-function putById(req, res) {
-  return res.status(200).json({ put: 'putById' });
-}
+// function putById(req, res) {
+//   return res.status(200).json({ put: 'putById' });
+// }
 
-speechByIdRouter.get('/', getById).put('/', putById);
+speechByIdRouter.get('/', getById);
+// .put('/', putById)
 
 export default speechByIdRouter;
--- setup/db-setup.test.js
+++ setup/db-setup.test.js
@@ -1,7 +1,9 @@
 import { jest } from '@jest/globals';
-import { setupTestDB } from './db.js';
+import setupDB, { setupTestDB, registerDbCollections } from './db.js';
 import { DB } from './../lib/models/db/index.js';
-
+import { UserAuth } from '../lib/models/userAuth/index.js';
+import { Crud } from '../lib/models/crud/index.js';
+import { get } from '../state.js';
 describe('setupTestDB', () => {
   const COLL_NAME = 'TestUsers';
   const DB_NAME = 'TestNlpDb';
@@ -9,17 +11,26 @@ describe('setupTestDB', () => {
     host: 'localhost',
     port: '27017',
   };
-  let testedDb;
+  let testedDb, connectSpy;
   beforeAll(async () => {
     process.env.MONGO_AUTH = false;
+    connectSpy = jest.spyOn(DB.prototype, 'connect');
   });
-  afterAll(async () => { 
+  afterEach(async () => { 
     await testedDb.close()
   })
-  it('calls DB.connect method', async () => {
-    const connectSpy = jest
-      .spyOn(DB.prototype, 'connect')
+  it('setupTestDB: calls DB.connect method', async () => {
     testedDb = await setupTestDB({ ...dbObj });
     expect(connectSpy).toHaveBeenCalledTimes(1);
   });
+  it('setupDB: calls DB.connect method', async () => {
+    testedDb = await setupDB({ ...dbObj });
+    expect(connectSpy).toHaveBeenCalledTimes(2);
+  });
+  it('registerDbCollections: sets state collections', async () => {
+    testedDb = await setupTestDB({ ...dbObj });
+    registerDbCollections(testedDb, true);
+    let userCollection = get('Users');
+    expect(userCollection instanceof UserAuth).toBe(true);
+  })
 });
