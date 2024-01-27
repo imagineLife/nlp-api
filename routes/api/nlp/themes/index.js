2cb09d4b3031f6d6a72e4668fcc06464ef8901e1
Eric Laursen <ericjlaursen@gmail.com>
2024-01-27 13:47:28 -0500

    remove caaching of theme data for now

--- routes/api/nlp/themes/index.js
+++ routes/api/nlp/themes/index.js
@@ -1,17 +1,10 @@
 import { getSentenceThemes } from './../../../../lib/index.js';
-import { stateObj, Users } from '../../../../state.js';
+import { Users } from '../../../../state.js';
 
 async function getAdHocThemes(req, res) {
   try {
     const userEmail = res?.locals?.jwt?.email;
-
-    if (!stateObj?.userData?.[userEmail]?.themes) {
-      let userThemes = await Users().getThemes({ email: res?.locals?.jwt?.email });
-      stateObj.userData[userEmail] = {};
-      stateObj.userData[userEmail].themes = userThemes;
-    }
-
-    const userThemes = stateObj.userData[userEmail].themes;
+    let userThemes = await Users().getThemes({ email: userEmail });
     const requestedText = decodeURI(req.params.txt);
     const sentenceThemes = getSentenceThemes(requestedText, userThemes);
     return res.status(200).json(sentenceThemes);
